072bc0c4b12e0d1f4fdf904a183726f5ec53d23e
diwakaraman <2023aspire53@gmail.com>
2025-03-26 01:12:44 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mangoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mangoose
+.connect("process.env.MONGO")
+.then(() => {
+  console.log('Connected to MongoDB!');
+}
+).catch((err) => {
+  console.log(err);
+}
+);
 const app = express();
 
 app.listen(3000, () => {
