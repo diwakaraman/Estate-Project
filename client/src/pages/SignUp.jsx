79448aa8b5ee5f67fe7e5dae63253f091c8b3548
diwakaraman <2023aspire53@gmail.com>
2025-03-29 22:35:08 +0530

    signup

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,19 @@
+import {useState} from 'react'
 import {Link} from 'react-router-dom'
 
+
 export default function SignUp() {
+
+const [formDate, setFormData] = useState({})
+const handlerChange = (e) => {
+  setFormData(
+    {
+      ...formDate,
+      [e.target.id]: e.target.value,
+    }
+  );
+};
+console.log(formDate);
   return (
     <div className='p-3 max-w-lg mx-auto'>
     <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
